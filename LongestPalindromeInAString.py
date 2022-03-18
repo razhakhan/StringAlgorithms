ff2e7f6e98a41c3c347c2e5f1341a77f08148f24
razhakhan <65012840+razhakhan@users.noreply.github.com>
2022-03-18 21:04:10 +0530

    Create LongestPalindromeInAString.py

---
+++ LongestPalindromeInAString.py
@@ -0,0 +1,49 @@
+#User function Template for python3
+
+class Solution:
+    def longestPalin(self, s):
+        start=0
+        end=1
+        n=len(s)
+        for i in range(1, n):
+            
+            # if the palindromic substring is even
+            low=i-1
+            high=i
+            while(low>=0 and high<n and s[low]==s[high]):
+                if(high-low+1>end):
+                    start=low
+                    end=high-low+1
+                low-=1
+                high+=1
+        
+            # if the palindromic substring is odd
+            low=i-1
+            high=i+1
+            while(low>=0 and high<n and s[low]==s[high]):
+                if(high-low+1>end):
+                    start=low
+                    end=high-low+1
+                low-=1
+                high+=1
+                
+        return s[start:start+end]
+            
+        
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        S = input()
+
+        ob = Solution()
+
+        ans = ob.longestPalin(S)
+
+        print(ans)
+# } Driver Code Ends
