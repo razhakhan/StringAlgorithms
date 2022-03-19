c96ba8f81f79e585683da1592f384aaba015247b
razhakhan <65012840+razhakhan@users.noreply.github.com>
2022-03-19 21:24:55 +0530

    Create PrintAllPermutationsOfString.py

---
+++ PrintAllPermutationsOfString.py
@@ -0,0 +1,19 @@
+def prints(arr,s,n):
+    l=[]
+    for i in range(s,n):
+        l.append(arr[i])
+    print("".join(l))
+    return
+    
+def permutation(arr, s, n):
+    if(s==n):
+        prints(arr,0,n)
+    for i in range(s, n):
+        arr[i], arr[s] = arr[s], arr[i]
+        permutation(arr, s+1, n)
+        arr[i], arr[s]=arr[s], arr[i]
+        
+st="abc"
+arr=list(st)
+n=len(st)
+permutation(arr,0,n)
