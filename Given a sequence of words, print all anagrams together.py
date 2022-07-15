89ba2a2e2e89588570555a99a0f3679a6126e180
Razha Khan <65012840+razhakhan@users.noreply.github.com>
2022-07-15 21:27:19 +0530

    Create Given a sequence of words, print all anagrams together.py

---
+++ Given a sequence of words, print all anagrams together.py
@@ -0,0 +1,73 @@
+#User function Template for python3
+
+class Solution:
+    def Anagrams(self, words, n):
+        '''
+        words: list of word
+        n:      no of words
+        return : list of group of anagram {list will be sorted in driver code (not word in grp)}
+        '''
+        dic={}
+        for word in words:
+            sword=sorted(word)
+            sword="".join(sword)
+            if sword in dic:
+                dic[sword].append(word)
+            else:
+                dic[sword]=[word]
+        
+        res=[]
+        for i in dic:
+            res.append(dic[i])
+        return res
+        #code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ =='__main__':
+    t= int(input())
+    for tcs in range(t):
+        n= int(input())
+        words=input().split()
+        
+        ob = Solution()
+        ans = ob.Anagrams(words,n)
+        
+        for grp in sorted(ans):
+            for word in grp:
+                print(word,end=' ')
+            print()
+
+# } Driver Code Ends
+
+'''
+
+Example 1:
+
+Input:
+N = 5
+words[] = {act,god,cat,dog,tac}
+Output:
+act cat tac 
+god dog
+Explanation:
+There are 2 groups of
+anagrams "god", "dog" make group 1.
+"act", "cat", "tac" make group 2.
+Example 2:
+
+Input:
+N = 3
+words[] = {no,on,is}
+Output: 
+no on
+is
+Explanation:
+There are 2 groups of
+anagrams "no", "on" make group 1.
+"is" makes group 2. 
+
+'''
